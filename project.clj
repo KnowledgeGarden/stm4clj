a30506d7acf5f8d24b6c62c22935fb245b43cbd4
Jack Park <jackpark@gmail.com>
2021-10-12 18:23:01 -0700

    added sql resource

---
+++ project.clj
@@ -0,0 +1,10 @@
+(defproject stm4clj "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
+            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [io.replikativ/datahike "0.3.6-SNAPSHOT"]
+                 [io.replikativ/datahike-postgres "0.3.1-SNAPSHOT"]]
+  :repl-options {:init-ns stm4clj.core}
+  :main stm4clj.core/foo)
---
+++ resources/tm.sql
@@ -0,0 +1,16 @@
+
+CREATE TABLE IF NOT EXISTS
+tuple (
+  entity    text UNIQUE,
+  reln      text NOT NULL,
+  val       text NOT NULL,
+  txid      timestamp NOT NULL,
+  "added"      boolean DEFAULT true,
+  PRIMARY KEY (lox, reln, val)
+);
+
+-- GRANT ALL PRIVILEGES ON tuple TO ??;
+
+CREATE INDEX IF NOT EXISTS tuple_idx
+  ON tuple (entity, reln, val);
+
