1c3304658af4f51addf7ae40b92ac3f0db98cafd
Jack Park <jackpark@gmail.com>
2021-10-16 12:48:02 -0700

    Starting db api

--- resources/tm.sql
+++ resources/tm.sql
@@ -1,3 +1,5 @@
+-- sketch of what it needs
+-- Datahike already uses a k-v datastore schema
 
 CREATE TABLE IF NOT EXISTS
 tuple (
--- src/stm4clj/postgres.clj
+++ src/stm4clj/postgres.clj
@@ -1,1 +1,25 @@
-(ns stm4clj.postgres)
+(ns stm4clj.postgres
+  (:require [datahike.api :as d]
+            [datahike-postgres.core]))
+
+;; hack adapted from https://github.com/replikativ/datahike-postgres
+(def cfg {:store {:backend :pg
+                  :host "localhost"
+                  :port 5432
+                  :user "alice"
+                  :password "foo"
+                  :dbname "example-db"}})
+
+(defn validate-db
+  "Test query; return true if valid"
+  []
+  (true) ;; for now
+  )
+
+(defn create-db
+  "Create a new database if one does not exist"
+  []
+  (if-not (validate-db)
+        (d/create-database cfg))
+  )
+
